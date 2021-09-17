eaac793ec9f4d761a0e129b6d13c55fb5cf77965
gurnameh-99 <gurunameh.work@gmail.com>
2021-09-17 17:03:51 +0530

    update

--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -20,125 +20,125 @@ const Schedule = () => {
                 <h6>Schedule SFD 2021</h6>
             </Row>
             <Row justify="center" style={{ paddingBlockEnd: '2em' }}>
-            <VerticalTimeline className="vertical-timeline-custom-line">
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{color: '#fff', background: "rgb(250 188 61)" }}
-                    contentArrowStyle={{ borderRight: '7px solid  rgb(250 188 61)' }}
-                    date="18th September 2021"
-                    iconClassName="vertical-timeline-element-icon bounce-in"
-                    iconStyle={{ background: 'rgb(250 188 61)', color: '#fff' }}
-                    icon={<WebIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Sonia Singla</h3>
-                    <h4 className="vertical-timeline-element-subtitle">10:30 - 11:30 am IST</h4>
-                    <p>
-                        Browsers and open source in the web domain.
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{color: '#fff', background: "rgb(231 231 231)" }}
-                    contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
-                    date="18th September 2021"
-                    iconStyle={{ background: 'rgb(250, 188, 61)', color: '#fff' }}
-                    icon={<BlockIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Gokul Alex</h3>
-                    <h4 className="vertical-timeline-element-subtitle">12:00 - 1:00 pm IST</h4>
-                    <p>
-                    Post Quantum Cryptography - Facets and Frontiers
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{color: '#fff', background: "rgb(231 231 231)" }}
-                    contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
-                    date="18th September 2021"
-                    iconStyle={{ background: 'rgb(250, 188, 61)', color: '#fff' }}
-                    icon={<AiIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Zarreen Naowal Reza</h3>
-                    <h4 className="vertical-timeline-element-subtitle">3:15 - 4:15 pm IST</h4>
-                    <p>
-                        Data Science, deep learning and the role of open source.
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{color: '#fff', background: "rgb(231 231 231)" }}
-                    contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
-                    date="18th September 2021"
-                    iconStyle={{ background: 'rgb(250, 188, 61)', color: '#fff' }}
-                    icon={<QnaIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Panel Discussion - Jivitesh Dhaliwal, Akshay Kumar, Pranav Kumar</h3>
-                    <h4 className="vertical-timeline-element-subtitle">5:00 - 6:00 pm IST</h4>
-                    <p>
-                        They all are here to talk about open source in the industry and answer all your questions regarding career or any other doubts you may have.
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    contentStyle={{color: '#fff', background: "rgb(231 231 231)" }}
-                    contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
-                    date="19th September 2021"
-                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    icon={<SecIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Madhava Jay</h3>
-                    <h4 className="vertical-timeline-element-subtitle">10:30 - 11:30 am IST</h4>
-                    <p>
-                    Open Source is your Destiny.
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    contentStyle={{color: '#fff', background: "rgb(231 231 231)" }}
-                    contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
-                    date="19th September 2021"
-                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    icon={<UiIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Adyasha Mohanty</h3>
-                    <h4 className="vertical-timeline-element-subtitle">12:00 - 1:00 pm IST</h4>
-                    <p>
-                        User Experience, User Interface design and FOSS.
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    contentStyle={{color: '#fff', background: "rgb(231 231 231)" }}
-                    contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
-                    date="19th September 2021"
-                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    icon={<EngineerIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Apaar Kamal</h3>
-                    <h4 className="vertical-timeline-element-subtitle">2:00 - 3:00 pm IST</h4>
-                    <p>
-                        SWE experience at google and FOSS in the insdustry.
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    contentStyle={{color: '#fff', background: "rgb(231 231 231)" }}
-                    contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
-                    date="19th September 2021"
-                    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-                    icon={<BoltIcon />}
-                >
-                    <h3 className="vertical-timeline-element-title">Lighting Talks - Madhav Gaba and Ritesh Aggarwal</h3>
-                    <h4 className="vertical-timeline-element-subtitle">3:15 - 4:15 pm IST</h4>
-                    <p>
-                        GSOC, Competetive programming, Open Source to Corporate.
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-                    icon={<FinishIcon />}
-                />
-            </VerticalTimeline>
+                <VerticalTimeline className="vertical-timeline-custom-line">
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ color: '#fff', background: "rgb(250 188 61)" }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(250 188 61)' }}
+                        date="18th September 2021"
+                        iconClassName="vertical-timeline-element-icon bounce-in"
+                        iconStyle={{ background: 'rgb(250 188 61)', color: '#fff' }}
+                        icon={<WebIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Sonia Singla</h3>
+                        <h4 className="vertical-timeline-element-subtitle">10:30 - 11:30 am IST</h4>
+                        <p>
+                            Browsers and open source in the web domain.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ color: '#fff', background: "rgb(231 231 231)" }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
+                        date="18th September 2021"
+                        iconStyle={{ background: 'rgb(250, 188, 61)', color: '#fff' }}
+                        icon={<BlockIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Gokul Alex</h3>
+                        <h4 className="vertical-timeline-element-subtitle">12:00 - 1:00 pm IST</h4>
+                        <p>
+                            Post Quantum Cryptography - Facets and Frontiers
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ color: '#fff', background: "rgb(231 231 231)" }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
+                        date="18th September 2021"
+                        iconStyle={{ background: 'rgb(250, 188, 61)', color: '#fff' }}
+                        icon={<AiIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Zarreen Naowal Reza</h3>
+                        <h4 className="vertical-timeline-element-subtitle">3:15 - 4:15 pm IST</h4>
+                        <p>
+                            Data Science, deep learning and the role of open source.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ color: '#fff', background: "rgb(231 231 231)" }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
+                        date="18th September 2021"
+                        iconStyle={{ background: 'rgb(250, 188, 61)', color: '#fff' }}
+                        icon={<QnaIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Panel Discussion - Jivitesh Dhaliwal, Akshay Kumar, Pranav Kumar</h3>
+                        <h4 className="vertical-timeline-element-subtitle">5:00 - 6:00 pm IST</h4>
+                        <p>
+                            A deep dive into open source.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        contentStyle={{ color: '#fff', background: "rgb(231 231 231)" }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
+                        date="19th September 2021"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        icon={<SecIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Madhava Jay</h3>
+                        <h4 className="vertical-timeline-element-subtitle">10:30 - 11:30 am IST</h4>
+                        <p>
+                            Open Source is your Destiny.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        contentStyle={{ color: '#fff', background: "rgb(231 231 231)" }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
+                        date="19th September 2021"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        icon={<UiIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Adyasha Mohanty</h3>
+                        <h4 className="vertical-timeline-element-subtitle">12:00 - 1:00 pm IST</h4>
+                        <p>
+                            User Experience, User Interface design and FOSS.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        contentStyle={{ color: '#fff', background: "rgb(231 231 231)" }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
+                        date="19th September 2021"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        icon={<EngineerIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Apaar Kamal</h3>
+                        <h4 className="vertical-timeline-element-subtitle">2:00 - 3:00 pm IST</h4>
+                        <p>
+                            SWE experience at google and FOSS in the insdustry.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        contentStyle={{ color: '#fff', background: "rgb(231 231 231)" }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(231 231 231)' }}
+                        date="19th September 2021"
+                        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                        icon={<BoltIcon />}
+                    >
+                        <h3 className="vertical-timeline-element-title">Lighting Talks - Madhav Gaba and Ritesh Aggarwal</h3>
+                        <h4 className="vertical-timeline-element-subtitle">3:15 - 4:15 pm IST</h4>
+                        <p>
+                            GSOC, Competetive programming, Open Source to Corporate.
+                        </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                        icon={<FinishIcon />}
+                    />
+                </VerticalTimeline>
             </Row>
         </Container>
     </>;
