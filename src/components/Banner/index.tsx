6ab91dc970dc9cedcf071c130b607bedc8d16757
Rispectech <ue208085.rishabhverma.it@gmail.com>
2022-08-19 17:42:54 +0530

    gather

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -11,38 +11,26 @@ import { Row } from "antd";
 const SLIDE_CHANGE_TIME_MS = 4000;
 
 const Banner = () => {
-    const [currentScene, setCurrentScene] = useCycle(
-        gatherTown,
-        Uiet
-    );
+  const [currentScene, setCurrentScene] = useCycle(Uiet);
 
-    useEffect(() => {
-        const timeOut = setTimeout(setCurrentScene, SLIDE_CHANGE_TIME_MS);
-        return () => clearTimeout(timeOut);
-    }, [currentScene, setCurrentScene]);
+  useEffect(() => {
+    const timeOut = setTimeout(setCurrentScene, SLIDE_CHANGE_TIME_MS);
+    return () => clearTimeout(timeOut);
+  }, [currentScene, setCurrentScene]);
 
-    return (
-        <>
-            <Row align="middle" justify="center">
-                <h1>Our Sponsors</h1>
-            </Row>
-            <div className="hero">
-                <IconHolder icon={currentScene.icon} text={currentScene.text} />
-                <ImageHolder
-                    img={currentScene.image1}
-                    className="animal-image animal-image__one"
-                />
-                <ImageHolder
-                    img={currentScene.image2}
-                    className="animal-image animal-image__two"
-                />
-                <ImageHolder
-                    img={currentScene.image3}
-                    className="animal-image animal-image__three"
-                />
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <Row align="middle" justify="center">
+        <h1>Our Sponsors</h1>
+      </Row>
+      <div className="hero">
+        <IconHolder icon={currentScene.icon} text={currentScene.text} />
+        <ImageHolder img={currentScene.image1} className="animal-image animal-image__one" />
+        <ImageHolder img={currentScene.image2} className="animal-image animal-image__two" />
+        <ImageHolder img={currentScene.image3} className="animal-image animal-image__three" />
+      </div>
+    </>
+  );
+};
 
 export default Banner;
